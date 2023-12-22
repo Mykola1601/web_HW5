73c7b949253ab821ee03b0ded71810e91c8c2cd9
Mykola1601 <nik160186@gmail.com>
2023-12-23 00:13:37 +0200

    to do higher, dict, loging

--- main.py
+++ main.py
@@ -3,59 +3,75 @@ import sys
 import json
 import aiohttp
 import asyncio
+import logging
 import platform
 import datetime
 
+logging.basicConfig(level=logging.INFO)
+
+valutes= ["EUR","USD"]
+
 def make_url(days):
     url = 'https://api.privatbank.ua/p24api/exchange_rates?json&date='+str(days.strftime("%d.%m.%Y"))
+    logging.info(f'url = {url}')
     return url
 
 
 async def index(session, url):
     async with session.get(url) as response:
         try:
+            logging.info(f'response.status == {response.status } ')
             if response.status == 200:
                 html = await response.json()
                 return html
         except:
-            print('exeption')
+            logging.info(f'exeption in index response ')
 
 
-async def main(days = 1):
+async def main(args):
+    logging.info(f'incomin args = {args}')
+    days = "1"
+    if len(args) >0:
+        days = args[0]
+        valutes.extend(args)
+    if not days.isdigit():
+        return 'wrong number of days'
+    if int(days) > 10:
+        return 'to mach days >>> 1-10'
+    days = int(days)
     today = datetime.datetime.now()
     today= today.date()
-    # days = int(input ("days>>>"))
+    logging.info(f'incomin days = {days}')
+    logging.info(f'valutes = {valutes}')
     res = []
+
     async with aiohttp.ClientSession() as session:
         try:
             for i in range(days):
                 delta = datetime.timedelta(days = i)
                 r = await index(session, make_url(today - delta))
+                dic = {  r['date'] : {}   }
                 ex = r['exchangeRate']
                 for i in ex:
-                    if i["currency"] ==  "EUR" :
-                        dic = {  r['date'] : {'EUR' : {'sale' : i['saleRate'] , 'purchase' : i['purchaseRate']} , "USD":{}  }   }
-                    if i["currency"] ==  "USD" :
-                        dic[r['date']]['USD'] = {'sale' : i['saleRate'] , 'purchase' : i['purchaseRate']} 
-                        res.append(dic)
+                    if i["currency"] in valutes :
+                        dic[r['date']].update({i["currency"] : {'sale' : i['saleRateNB'] , 'purchase' : i['purchaseRateNB']} })
+                res.append(dic)
+            # logging.info(f'respons finish ')    
             return res
         except aiohttp.client_exceptions.ClientConnectorError: 
-            print('bad URL')
+            logging.info(f'except == {aiohttp.client_exceptions.ClientConnectorError} ')
         except AssertionError: 
-            print('bad URL AssertionError')
+            logging.info(f'except == {AssertionError} ')
 
 
 if __name__ == "__main__":
-    user_input = 1
-    if len(sys.argv) > 1:
-        user_input = int(sys.argv[1])
-    print(user_input)
-    if user_input >= 10:
-        print('input 1-10 days')
-        exit()
+    logging.info(f'main == __name__')
+    logging.info(f'input args = {sys.argv}')
     if platform.system() == 'Windows':
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    r = asyncio.run(main(user_input))
+    r = asyncio.run(main(sys.argv[1:]))
     r = json.dumps(r, indent=2)
     print(r)
-    
+
+
+
--- server.py
+++ server.py
@@ -1,24 +1,56 @@
-import platform
 
-import aiohttp
+
+import json
 import asyncio
-import requests
+import logging
+import websockets
+from datetime import datetime
+from websockets import WebSocketServerProtocol
+from websockets.exceptions import ConnectionClosedOK
+from main import main as exchange
 
-response = requests.get('https://api.privatbank.ua/p24api/exchange_rates?json&date=01.12.2023')
-exchange_rate = response.json()
-print(exchange_rate)
-response = requests.get('https://api.privatbank.ua/p24api/exchange_rates?json&date=02.12.2023')
-exchange_rate = response.json()
-print(exchange_rate)
-# response = requests.get('https://api.privatbank.ua/p24api/exchange_rates?json&date=03.12.2023')
-# exchange_rate = response.json()
-# print(exchange_rate)
-# response = requests.get('https://api.privatbank.ua/p24api/exchange_rates?json&date=04.12.2023')
-# exchange_rate = response.json()
-# print(exchange_rate)
-# response = requests.get('https://api.privatbank.ua/p24api/exchange_rates?json&date=05.12.2023')
-# exchange_rate = response.json()
-# print(exchange_rate)
-# response = requests.get('https://api.privatbank.ua/p24api/exchange_rates?json&date=06.12.2023')
-# exchange_rate = response.json()
-# print(exchange_rate)
+logging.basicConfig(level=logging.INFO)
+
+
+class Server:
+    clients = set()
+
+    async def register(self, ws: WebSocketServerProtocol):
+        ws.name = datetime.now().time()
+        self.clients.add(ws)
+        logging.info(f'{ws.remote_address} connects')
+
+    async def unregister(self, ws: WebSocketServerProtocol):
+        self.clients.remove(ws)
+        logging.info(f'{ws.remote_address} disconnects')
+
+    async def send_to_clients(self, message: str):
+        if self.clients:
+            [await client.send(message) for client in self.clients]
+
+    async def ws_handler(self, ws: WebSocketServerProtocol):
+        await self.register(ws)
+        try:
+            await self.distrubute(ws)
+        except ConnectionClosedOK:
+            pass
+        finally:
+            await self.unregister(ws)
+
+    async def distrubute(self, ws: WebSocketServerProtocol):
+        async for message in ws:
+            if "exchange" in message:
+                args = (message.split(' '))
+                args.append("1")
+                message = await exchange(args[1:])  
+                message = f"money for {args[1]} days {message}"
+            await self.send_to_clients(f"Name {ws.name}: >>>  {message}")
+
+
+async def main():
+    server = Server()
+    async with websockets.serve(server.ws_handler, 'localhost', 8080):
+        await asyncio.Future()  # run forever
+
+if __name__ == '__main__':
+    asyncio.run(main())
